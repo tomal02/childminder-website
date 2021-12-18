a85922a8b7ed7e4cf4b96a7735ec820e8d8e8759
Tom Lee <tomalee2002@gmail.com>
2021-12-18 19:25:27 +0000

    creates Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </div>
+  );
+};
+
+export default Navbar;
