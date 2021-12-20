2ba42ef84b14a99ebc687a14c579a9f144290e7f
Tom Lee <tomalee2002@gmail.com>
2021-12-20 04:31:45 +0000

    fixed bug with component rendering

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={Home} />
-          <Route path="/contact" element={Contact} />
-          <Route path="/about" element={About} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
