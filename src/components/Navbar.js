301fdaca6ef5b6a3e8940b7ea7ec9c1e4863c19c
Tom Lee <tomalee2002@gmail.com>
2021-12-20 04:10:45 +0000

    adds basic css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
+import './Navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
