f1de2b640b0af50ed2593b39b3039fabd9fb5339
Tom Lee <tomalee2002@gmail.com>
2021-12-20 03:22:21 +0000

    creates about page and adds it to navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={Home} />
           <Route path="/contact" element={Contact} />
+          <Route path="/about" element={About} />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,9 @@ const Navbar = () => {
       <li>
         <Link to="/contact">Contact</Link>
       </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
     </div>
   );
 };
