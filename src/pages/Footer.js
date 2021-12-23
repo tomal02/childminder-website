2c182c1ff0db639045ada4adb98d21c4964d034c
Tom Lee <tomalee2002@gmail.com>
2021-12-23 16:32:15 +0000

    adds Molesey Childminder info to Footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,7 +1,7 @@
 import './Footer.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSnapchat, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
@@ -13,12 +13,10 @@ const Footer = () => {
           <div class="container">
             <div class="row" id="rowier">
               <div class="col-md-6 item text">
-                <h3>Company Name</h3>
+                <h3>Molesey Childminder</h3>
                 <p>
-                  Praesent sed lobortis mi. Suspendisse vel placerat ligula.
-                  Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam
-                  quis tristique lectus. Aliquam in arcu eget velit pulvinar
-                  dictum vel in justo.
+                  An Ofsted rated 'outstanding' childminder, providing children
+                  with a safe and secure environment to learn and play.
                 </p>
               </div>
               <div class="col item social">
@@ -42,8 +40,6 @@ const Footer = () => {
           </div>
         </footer>
       </div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     </div>
   );
 };
