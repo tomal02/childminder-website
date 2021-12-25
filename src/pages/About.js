3faf9d7603234456a9e554b90dbddaa8c540ea88
Tom Lee <tomalee2002@gmail.com>
2021-12-25 00:18:28 +0000

    adds relevant content to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,8 +7,31 @@ const About = () => {
     <section id="about">
       <img src={childPhoto} alt="unsplash-img" />
       <div>
-        <h3>ABOUT</h3>
-        <p>This is the about section</p>
+        <h3>ABOUT ME</h3>
+        <p>
+          <strong>GRADED OUTSTANDING 2020 & 2016</strong>
+          <br /> <br />
+          I am happily married Mum with 2 boys (2002 & 2012) My setting has a
+          large back garden with a secure decked area that has a wide selection
+          of sit on & ride on bikes / cars sandpit and messy / safe play. We
+          have a fabulous mud kitchen area, with a large bench and lots of
+          natural resources to explore.
+          <br />
+          <br />
+          During non Covid restriction times, we attend playgroups within the
+          local area, do walks along the river and visits to local woods for
+          exploring.
+          <br /> <br />I have my own car, a clean license with business car
+          insurance, I use / supply my own car seats, suitable for all different
+          age children.
+          <br /> <br />I am able to accept childcare vouchers and am already
+          registered with numerous companies.
+          <br /> <br />
+          <strong>
+            If you are looking for a nurturing, fun, safe and homely environment
+            for your child, then please feel free to contact me.
+          </strong>
+        </p>
       </div>
     </section>
   );
